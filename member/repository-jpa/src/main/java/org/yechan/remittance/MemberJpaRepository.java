5821ff4069b2f3a756b3f5d8e33b9a1128180f88
YeaChan05 <qkenrdl05@gmail.com>
2026-01-05 19:21:49 +0900

    feat(member): 멤버 JPA 레포지토리 인터페이스 추가

---
+++ member/repository-jpa/src/main/java/org/yechan/remittance/MemberJpaRepository.java
@@ -0,0 +1,8 @@
+package org.yechan.remittance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long>{
+
+}
+
