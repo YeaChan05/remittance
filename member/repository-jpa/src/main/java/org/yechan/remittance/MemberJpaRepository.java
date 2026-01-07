aa1a5f7351af564137a440ffe20dd5faadf6290f
YeaChan05 <qkenrdl05@gmail.com>
2026-01-07 13:00:03 +0900

    chore(member): repository-jpa 모듈 및 관련 파일 삭제

--- member/repository-jpa/src/main/java/org/yechan/remittance/MemberJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.yechan.remittance;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
-  Optional<MemberEntity> findByEmail(String email);
-}
-
