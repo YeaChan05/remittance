9cc042783a64d2ff0dc54cf5232a2100f00296a7
YeaChan05 <qkenrdl05@gmail.com>
2026-01-07 10:04:43 +0900

    refactor(application): 패키지 구조 및 경로 변경

--- aggregate/src/main/java/org/yechan/application/AggregateApplication.java
+++ aggregate/src/main/java/org/yechan/application/AggregateApplication.java
@@ -1,4 +1,4 @@
-package org.yechan.aggregate;
+package org.yechan.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- aggregate/src/main/java/org/yechan/application/config/AggregateSecurityConfiguration.java
+++ aggregate/src/main/java/org/yechan/application/config/AggregateSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package org.yechan.aggregate;
+package org.yechan.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
