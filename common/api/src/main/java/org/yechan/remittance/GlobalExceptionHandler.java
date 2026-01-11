3beece5f2bc1d18fb1884d9e36cfd221432dd4d2
YeaChan05 <qkenrdl05@gmail.com>
2026-01-11 23:13:17 +0900

    feat(api): 예외 핸들러에 로그 추가

--- common/api/src/main/java/org/yechan/remittance/GlobalExceptionHandler.java
+++ common/api/src/main/java/org/yechan/remittance/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package org.yechan.remittance;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,13 +11,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<?> handleBusinessException(BusinessException e) {
+    log.error(Arrays.toString(e.getStackTrace()));
     return ResponseEntity.status(e.getStatus().toHttpStatus()).body(e.getMessage());
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   ResponseEntity<?> handleValidationException(MethodArgumentNotValidException e) {
+    log.error(Arrays.toString(e.getStackTrace()));
     return ResponseEntity.badRequest().body(e.getMessage());
   }
 }
