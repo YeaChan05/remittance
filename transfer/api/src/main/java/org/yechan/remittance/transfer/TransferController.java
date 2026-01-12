3ffecee3f6481599a52885ac6ce941defc9bfc1d
YeaChan05 <qkenrdl05@gmail.com>
2026-01-12 22:42:07 +0900

    feat(transfer): 송금 조회 컨트롤러 메서드 추가

--- transfer/api/src/main/java/org/yechan/remittance/transfer/TransferController.java
+++ transfer/api/src/main/java/org/yechan/remittance/transfer/TransferController.java
@@ -1,17 +1,24 @@
 package org.yechan.remittance.transfer;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.yechan.remittance.LoginUserId;
 import org.yechan.remittance.transfer.dto.TransferRequest;
+import org.yechan.remittance.transfer.dto.TransferQueryResponse;
 
 @RequestMapping("/transfers")
 @RestController
 record TransferController(
-    TransferCreateUseCase transferCreateUseCase
+    TransferCreateUseCase transferCreateUseCase,
+    TransferQueryUseCase transferQueryUseCase
 ) implements TransferApi {
 
   @Override
@@ -21,5 +28,24 @@ record TransferController(
       @PathVariable String idempotencyKey,
       @RequestBody TransferRequest props) {
     return transferCreateUseCase.transfer(memberId, idempotencyKey, props);
+  }
+
+  @Override
+  @GetMapping
+  public TransferQueryResponse query(
+      @LoginUserId Long memberId,
+      @RequestParam Long accountId,
+      @RequestParam(required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+      @RequestParam(required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to,
+      @RequestParam(required = false) Integer limit
+  ) {
+    List<? extends TransferModel> transfers = transferQueryUseCase.query(
+        memberId,
+        accountId,
+        new TransferQueryCondition(from, to, limit)
+    );
+    return TransferQueryResponse.from(transfers);
   }
 }
