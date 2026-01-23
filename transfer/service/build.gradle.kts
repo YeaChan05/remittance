29fe14b578ba9637523947b0f186954c0522c8d4
YeaChan05 <qkenrdl05@gmail.com>
2026-01-23 11:28:24 +0900

    refactor(transfer): Jackson Databind 의존성 추가

--- transfer/service/build.gradle.kts
+++ transfer/service/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation(project(":transfer:exception"))
     implementation(project(":account:infrastructure"))
     implementation(project(":member:infrastructure"))
+    implementation("com.fasterxml.jackson.core:jackson-databind")
     // spring tx
     implementation("org.springframework:spring-tx")
 }
